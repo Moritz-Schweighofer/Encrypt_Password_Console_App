f3e3ceebfc0464f84ebcd726c138bc30ba903c18
Moritz-Schweighofer <56548437+Moritz-Schweighofer@users.noreply.github.com>
2019-11-26 10:53:37 +0100

    Projektdateien hinzufügen.

---
+++ General Functions/RSAController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Public_Private_Key_Encryptor
+{
+    public class RSAController
+    {
+        private static UnicodeEncoding _encoder = new UnicodeEncoding();
+
+        public RSAController()
+        {
+            //Pass
+        }
+
+        public static string Encrypt(string data, string _public_key)
+        {
+            try
+            {
+                var rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(_public_key);
+                var dataToEncrypt = _encoder.GetBytes(data);
+
+                var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false);
+                var encryptedString = Convert.ToBase64String(encryptedByteArray);
+
+                return encryptedString;
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("Error With Encrypting the Password: " + ex.Message);
+                return "";
+            }
+        }
+
+        public static string Decrypt(string data, string _private_key)
+        {
+            try
+            {
+                var rsa = new RSACryptoServiceProvider();
+                var dataByte = Convert.FromBase64String(data);
+
+                rsa.FromXmlString(_private_key);
+                var decryptedByte = rsa.Decrypt(dataByte, false);
+
+                return _encoder.GetString(decryptedByte);
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("Error With Decrypting the Password: " + ex.Message);
+                return "";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Public_Private_Key_Encryptor;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Encrypt_Password_Console_App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string _publicKey = "<RSAKeyValue><Modulus>i6BFZEiowigxEIV/2SUNUTjgslJas0Qe5pqi1qG6CZvyS25ruyZ67FHQw6mSHg22tZ/sfTBWSuS2LU+9QaqVuw==" +
+                "</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+
+            Console.WriteLine("Please enter the Password you want to have Encrypted: ");
+
+            string passwordPlain = Console.ReadLine();
+
+            string passwordEncrypted = RSAController.Encrypt(passwordPlain, _publicKey);
+
+            Console.WriteLine("");
+            Console.WriteLine("Your Encrypted Password is: " + passwordEncrypted);
+
+            string m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(m_exePath + "\\Encrypted.txt", true))
+                {
+                    sw.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                    sw.WriteLine("  ");
+                    sw.WriteLine("  {0}", passwordEncrypted);
+                    sw.WriteLine("-------------------------------");
+                }
+
+                Console.WriteLine("Note: the Password has also been saved into the Encryptet.txt");
+            }
+            catch (Exception)
+            {
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Pressy Any Key to exit :)");
+
+            Console.ReadKey();
+        }
+    }
+}
